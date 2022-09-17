fb00319d6743744fd9ec7771fd1d3cdf08756ea7
niclast7611 <niclast7611@gmail.com>
2022-09-17 16:26:24 -0700

    keep the green dots goin

--- src/Form.js
+++ src/Form.js
@@ -4,6 +4,9 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+
 
 export default function Form() {
   return (
@@ -11,28 +14,33 @@ export default function Form() {
       <FormControl>
         <TextField
               id="outlined-required"
-              label="Age"
-            />
+              label="Age"/>
         <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
-        <RadioGroup row name="row-radio-buttons-group">
-        <FormControlLabel value="female" control={<Radio />} label="Female" />
-        <FormControlLabel value="male" control={<Radio />} label="Male" />
-      </RadioGroup>
-      </FormControl>
+          <RadioGroup row name="row-radio-buttons-group">
+            <FormControlLabel value="female" control={<Radio />} label="Female" />
+            <FormControlLabel value="male" control={<Radio />} label="Male" />
+          </RadioGroup>
       <FormLabel id="demo-radio-buttons-group-label">Height</FormLabel>
-      <TextField
+        <TextField
               id="outlined-required"
-              label="Feet"
-            />
-            <TextField
+              label="Feet"/>
+        <TextField
               id="outlined-required"
-              label="Inches"
-            />
-            <FormLabel id="demo-radio-buttons-group-label">Weight</FormLabel>
-      <TextField
-              id="outlined-required"
-              label="Pounds"
-            />
+              label="Inches"/>
+      <FormLabel id="demo-radio-buttons-group-label">Weight</FormLabel>
+        <TextField
+                id="outlined-required"
+                label="Pounds"/>
+      <FormLabel id="demo-simple-select-label">Activity</FormLabel>
+        <Select
+          label="Activity">
+            {/* need to change values and add default value */}
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+
+      </FormControl>
     </div>
   )
 }
