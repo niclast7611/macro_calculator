3bdcdd41a1c353ad18edceed847e36276b7a03fe
niclast7611 <niclast7611@gmail.com>
2022-09-21 16:43:16 -0700

    all pieces to form are in

--- src/Form.js
+++ src/Form.js
@@ -1,49 +1,96 @@
-import TextField from '@mui/material/TextField';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import Card from '@mui/material/Card';
-import Button from '@mui/material/Button';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormLabel from "@mui/material/FormLabel";
 
 export default function Form() {
   return (
-    <div className="grid h-screen place-items-center">
-      <Card 
-      variant="outlined" 
-      className='h-fit w-[40rem] justify-center p-6 text-center' 
-    >
-           <h1 className='text-3xl pb-5' 
-              >Macro Calculator</h1>
-
-        <FormControl className='pb-5'>
-          <TextField
-                label="Age"/>
-
-            <RadioGroup row name="row-radio-buttons-group">
-              <FormControlLabel value="female" control={<Radio />} label="Female" />
+    <div className="h-100 mt-[30vh]">
+      <Card
+        sx={{
+          width: "content",
+          height: "content",
+          textAlign: "center",
+        }}
+        variant="outlined"
+      >
+        <CardContent>
+          <h1 className="mb-2 text-3xl">Macro Calculator</h1>
+          <div className="flex my-2 justify-center">
+            <h2 className="mr-4 text-xl leading-loose">Gender</h2>
+            <RadioGroup row>
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
               <FormControlLabel value="male" control={<Radio />} label="Male" />
             </RadioGroup>
-        <div className='flex-row justify-between pb-5'>
-
-          <TextField label="Height (Feet)"/>
-
-          <TextField label="Height (Inches)"/>
-  
-        </div>
-
-          <TextField label="Weight in Pounds"/>
-
-
-        <RadioGroup row>
-          <FormControlLabel value="12" control={<Radio />} label="Loose" />
-          <FormControlLabel value="14" control={<Radio />} label="Maintain" />
-          <FormControlLabel value="15" control={<Radio />} label="Gain" />
-        </RadioGroup>
-
-        <Button variant="contained" color="success">Submit</Button>
-        </FormControl>
+          </div>
+          <TextField label="Age" variant="outlined" />
+          <FormLabel>Weight</FormLabel>
+          <TextField label="lbs" variant="outlined" />
+          <FormLabel>Height</FormLabel>
+          <TextField label="ft" variant="outlined" />
+          <TextField label="in" variant="outlined" />
+          <FormLabel id="demo-row-radio-buttons-group-label">
+            Activity Level
+          </FormLabel>
+          <RadioGroup row>
+            <FormControlLabel
+              value="1.2"
+              control={<Radio />}
+              label="Sedentary (little or no exercise)"
+            />
+            <FormControlLabel
+              value="1.375"
+              control={<Radio />}
+              label="Lightly active (exercise 1–3 days/week)"
+            />
+            <FormControlLabel
+              value="1.55"
+              control={<Radio />}
+              label="Moderately active (exercise 3–5 days/week)"
+            />
+            <FormControlLabel
+              value="1.725"
+              control={<Radio />}
+              label="Active (exercise 6–7 days/week)"
+            />
+            <FormControlLabel
+              value="1.9"
+              control={<Radio />}
+              label="Very active (hard exercise 6–7 days/week)"
+            />
+          </RadioGroup>
+          <FormLabel id="demo-row-radio-buttons-group-label">
+            Your Goals
+          </FormLabel>
+          <RadioGroup row>
+            <FormControlLabel
+              value="lose"
+              control={<Radio />}
+              label="Lose Weight"
+            />
+            <FormControlLabel value="gain" control={<Radio />} label="Gain" />
+            <FormControlLabel
+              value="maintain"
+              control={<Radio />}
+              label="Maintain"
+            />
+          </RadioGroup>
+        </CardContent>
+        <CardActions>
+          <Button size="medium" variant="outlined">
+            Calculate
+          </Button>
+        </CardActions>
       </Card>
     </div>
-  )
+  );
 }
